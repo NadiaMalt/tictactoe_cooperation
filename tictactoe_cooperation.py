451e3bc7732418be768130734c65772ab98a8012
Nadia Maltezou <maltezou.nadia@gmail.com>
2022-03-27 18:27:11 +0200

    final nadia

--- tictactoe_cooperation.py
+++ tictactoe_cooperation.py
@@ -6,12 +6,13 @@
   # check rows
 # check tie
   # if the board is full and there is no winner
-# flip the players
 
-# --Global variables--
+
 
 # board
 print("lets start this game")
+
+# --Global variables--
 board = "____________________"
 
 # If game is still going
@@ -29,13 +30,13 @@ def play_game():
   # prints the initial board
   display_board()
 
+  # while the game_still_going is true the play_game function calls the players_move ,pc_move and evaluate_game
   while game_still_going: 
 
     players_move()
     pc_move()
     evaluate_game()
-    flip_player()
-
+    
 # the game has ended
   if winner == "X" or winner == "O":
     print(winner, "has won and is great")
@@ -46,11 +47,15 @@ def play_game():
 def players_move():
     global board
     position_player = int(input("Chose a position between 1-20:"))
+    # while one of the three conditions are happening i want the player to input again
     while position_player < 1 or position_player > 20 or board[position_player-1] != "_":
+      # if these two conditions are happening i want the following message
       if position_player < 1 or position_player > 20:
         print("can you please give a position in the correct range? ;)")
+      # the position of the player is alredy taken i want the following message
       elif board[position_player-1] != "_":
         print("position already taken")
+      # i want the player to input again all the time we are in the while
       position_player = int(input("Chose a position between 1-20:"))
     else:
       board=board[:position_player-1]+board[position_player-1].replace('_','X')+board[position_player:] 
@@ -67,7 +72,6 @@ def pc_move():
         position_pc = randrange(1,20)                
     if board[position_pc-1] == "_":
         board=board[:position_pc-1]+board[position_pc-1].replace('_','O')+board[position_pc:]
-        
     print(board)
 
 def evaluate_game():
@@ -94,9 +98,10 @@ def check_if_win():
     return winner, game_still_going
     
 def check_if_tie():
-  return
+  global game_still_going
+  if "_" not in board:
+    game_still_going = False
+    return
 
-def flip_player():
-  return
-  
+# this is the first function i call in orde to start the game  
 play_game()
